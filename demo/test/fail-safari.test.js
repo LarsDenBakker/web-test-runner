eae84060c9278ff97b6163b835f3e4de128ec942
Lars den Bakker <larsdenbakker@gmail.com>
2020-05-29 00:00:27 +0200

    Improve demo test

--- demo/test/fail-safari.test.js
+++ demo/test/fail-safari.test.js
@@ -2,6 +2,6 @@ import { expect } from './chai.js';
 
 it('only fails on safari', () => {
   if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
-    throw new Error('This should fail on safari');
+    expect(true).to.be.false;
   }
 });
